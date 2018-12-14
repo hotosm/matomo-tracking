124de131712a3d4c1c75fa3c3dfae693ca60e3dc
Dakota Benjamin <dakota.benjamin@hotosm.org>
2018-12-14 14:24:52 -0500

    Make script contained in single file

--- tracking.js
+++ tracking.js
@@ -1,3 +1,56 @@
+/* Add div content */
+var html = `
+<div id="optout-contents">
+  <p>This site uses Matomo to analyze traffic and help us improve your user experience. It is processing your IP address and stores cookies on your browser for 13 months. Those data are only processed by us and our web hosting platform. You may choose to opt of of this below. </p>
+  <p><a href="hotosm.org/privacy"> Learn more about our Privacy Policy</a></p>
+  <p>
+    <input type="checkbox" id="optout" />
+    <label for="optout"><strong></strong></label>
+  </p>
+</div>
+<div class="close-button" id="optout-close">Close</div>
+`;
+
+var form =  document.getElementById('optout-form');
+if (typeof(form) != 'undefined' && form != null)
+{
+  form.innerHTML = html;
+
+  var formContents = document.getElementById('optout-contents');
+  var formContentsP = formContents.getElementsByTagName("P");
+  var formClose = document.getElementById('optout-close');
+  
+  if (!localStorage.getItem("optout-closed")) {
+    form.style.display = 'grid';
+    form.style.position = 'fixed';
+    form.style.bottom = '0%';
+    form.style.left = '0%';
+    form.style.width = '10$';
+    form.style.padding = '0 15%';
+    form.style.backgroundColor = '#2C3038';
+    form.style.zIndex = '1001';
+    form.style.gridTemplateColumns = '90% 10%';
+
+    formContents.style.gridColumn = '1';
+    formContents.style.paddingTop = '14px';
+
+    for (i = 0; i < formContentsP.length; i++) {
+      formContentsP[i].style.color = '#E1E0E0';
+      formContentsP[i].style.textAlign = 'center';
+    }
+
+    formClose.style.gridColumn = '2';
+    formClose.style.color = '#E1E0E0';
+    formClose.style.fontSize = '1rem';
+    formClose.style.textAlign = 'right';
+    formClose.style.padding = '14px 0 11px 24px';
+    formClose.style.marginLeft = '16px';
+    formClose.style.cursor = 'pointer';
+    formClose.style.zIndex = '1002';
+  }
+
+}
+
 // Matomo tracker
 var _paq = _paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
@@ -46,12 +99,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   setOptOutText(optOut);
 
   document.getElementById('optout-close').onclick = function() {
-    document.getElementById("optout-form").classList.remove("optout-show");
+    form.style.display = 'none';
     localStorage.setItem("optout-closed", "true");
   };
-
-  if (!localStorage.getItem("optout-closed")) {
-    document.getElementById("optout-form").classList.add("optout-show");
-  }
-
 });
