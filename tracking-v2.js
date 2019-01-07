e322c5d0f1138ffac31b60b66d897c0bd2c33585
Dakota Benjamin <dakota.benjamin@hotosm.org>
2019-01-06 22:22:07 -0500

    Update hotjar code

--- tracking-v2.js
+++ tracking-v2.js
@@ -147,14 +147,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   };
 });
 
-// <!-- Hotjar Tracking Code for hotosm.org -->
-(function(h,o,t,j,a,r){
-    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:1137117,hjsv:6};
-    a=o.getElementsByTagName('head')[0];
-    r=o.createElement('script');r.async=1;
-    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-    a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-// <!-- End of Hotjar code -->
-
+<!-- Hotjar Tracking Code for https://tasks.hotosm.org -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1147905,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
